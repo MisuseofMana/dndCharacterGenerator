96f7d6b2a8558455f2132cfd943c6cdc5c3044a9
Sean <contact@seanyager.com>
2020-05-13 17:42:17 -0500

    images! and new name API

--- main.js
+++ main.js
@@ -3,8 +3,14 @@
 let classes = [];
 let currentClass = [];
 
-const getClasses = (path) => {
+const getClasses = () => {
     fetch('https://www.dnd5eapi.co/api/classes')
+    .then(res => res.json())
+    .then(gotten => classes = gotten)
+} 
+
+const getRandomName = () => {
+    fetch('http://names.drycodes.com/10?nameOptions=boy_names&&na')
     .then(res => res.json())
     .then(gotten => classes = gotten)
 } 
