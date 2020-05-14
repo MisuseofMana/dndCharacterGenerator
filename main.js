de988c6743db72a586aa8118b5b881260bb65a65
Sean <contact@seanyager.com>
2020-05-14 18:34:36 -0500

    updating css

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ let fetchData = [
     {type:'name', gotInfo:undefined}, 
     {type:'baseData', gotInfo:undefined},
     {type:'races', gotInfo:undefined},
-    {type:'raceData', gotInfo:undefined}
+    {type:'raceData', gotInfo:undefined},
+    {type:'equip', gotInfo:undefined},
 
 ]
 
@@ -36,7 +37,7 @@ const getData = (path, context) => {
 function getCharacter(whosClicked) {    
     getData('https://www.dnd5eapi.co/api/classes/' + whosClicked, 'baseData');
     getData('https://www.dnd5eapi.co/api/races/', 'races')
-    getData('https://randomuser.me/api/?inc=gender,name', 'name');
+    getData('https://randomuser.me/api/?inc=gender,name&&nat=us,dk,fr,gb,br,no', 'name');
 }
 
 function makeCharacter(whatToRun) {
@@ -49,11 +50,27 @@ function makeCharacter(whatToRun) {
         let domClass = document.getElementById('class');
         domClass.innerText = basehandler.name;
 
-        let domApperance = document.getElementById('appearance');
         let domHitDie = document.getElementById('hitdie');
+        domHitDie.innerText = `d${basehandler.hit_die}`;
+
         let domProf = document.getElementById('prof');
+        domProf.innerHTML = '';
+        for(const item of basehandler.proficiencies) {
+            let newItem = document.createElement('p');
+            domProf.appendChild(newItem);
+            newItem.innerText = item.name;
+        }
+
         let domSaveThrow = document.getElementById('saveThrow');
-        let domEquip = document.getElementById('equipment');
+        domSaveThrow.innerHTML = '';
+        for(const item of basehandler.saving_throws) {
+            let newItem = document.createElement('p');
+            domSaveThrow.appendChild(newItem);
+            newItem.innerText = item.name;
+        }
+
+        getData('https://www.dnd5eapi.co' + basehandler.starting_equipment.url, 'equip');
+
     }
     else if(whatToRun === 'races') {
         let racehandlerIndex = fetchData.findIndex(o => o.type === 'races');
@@ -75,7 +92,6 @@ function makeCharacter(whatToRun) {
 
 
     }
-
     else if (whatToRun === 'raceData') {
         let raceDatahandlerIndex = fetchData.findIndex(o => o.type === 'raceData');
         let raceDatahandler = fetchData[raceDatahandlerIndex].gotInfo;
@@ -83,7 +99,21 @@ function makeCharacter(whatToRun) {
         console.log(raceDatahandler);
 
         let alignSugg = document.getElementById('alignment');
-        alignSugg.innerText
+        alignSugg.innerText = raceDatahandler.alignment;
+    }
+    else if (whatToRun === 'equip') {
+        let equiphandlerIndex = fetchData.findIndex(o => o.type === 'equip');
+        let equiphandler = fetchData[equiphandlerIndex].gotInfo;
+        console.log('Race Data Handler');
+        console.log(equiphandler);
+
+        let domEquip = document.getElementById('equipment');
+        domEquip.innerHTML = '';
+        for(const gear of equiphandler.starting_equipment) {
+            let newItem = document.createElement('p');
+            domEquip.appendChild(newItem);
+            newItem.innerText = `${gear.item.name} x${gear.quantity}`;
+        }
     }
     else if (whatToRun === 'name') {
         let namehandlerIndex = fetchData.findIndex(o => o.type === 'name');
@@ -93,10 +123,9 @@ function makeCharacter(whatToRun) {
 
         let domName = document.getElementById('name');
         domName.innerText = namehandler.results[0].name.first + ' ' + namehandler.results[0].name.last;
-
     }
 }
 
 function randomNum(max){
-    return Math.floor(Math.random() * max - 1);
+    return Math.floor(Math.random() * (max - 1) + 1);
 }
